d7490c617a4bdb16d08a8d62bb2153118d0befbd
SebastianWBR <sebastian.radja@mhs.unsoed.ac.id>
2022-09-30 22:53:02 +0700

    Initial commit

---
+++ app/src/main/java/com/bastian/tugas3prak/succes_reset_password.java
@@ -0,0 +1,14 @@
+package com.bastian.tugas3prak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class succes_reset_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_succes_reset_password);
+    }
+}
